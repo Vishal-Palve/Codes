e1538a266752b6580a31f99e8a6c52a9147b7871
Vishal Palve <vishalpalve1011@gmail.com>
2025-12-11 13:35:22 +0530

    v

--- src/LeetCode/Q148.java
+++ src/LeetCode/Q148.java
@@ -4,7 +4,7 @@ class ListNode {
       int val;
       ListNode next;
       ListNode() {}
-    ListNode(int val) { this.val = val; }
+      ListNode(int val) { this.val = val; }
       ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
 public class Q148 {
